8c7fba1cca857977dceb7750a5e0af6b8508427e
Mit Desai <mitdesai4703@gmail.com>
2025-12-20 13:58:35 +0530

    commit changes

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,5 +1,5 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-11-13'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-12-20'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
